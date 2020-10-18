59a928fe53dbf8716080ba13f411fcf638cbc092
atoht <atoht9527@gmail.com>
2020-10-18 22:32:45 +0900

    Initial Commit

---
+++ weiboDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weiboDemo
+//
+//  Created by atoht on 2020/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weiboDemo/weiboDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weiboDemoApp.swift
+//  weiboDemo
+//
+//  Created by atoht on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct weiboDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
